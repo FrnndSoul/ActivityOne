4c86fb85dcdc5b8fd1621b05fa1c21abdbe51503
David Gubat <davidgubat.dg@gmail.com>
2023-08-20 14:34:57 +0800

    d

--- ActivityOne/Login.Designer.cs
+++ ActivityOne/Login.Designer.cs
@@ -141,7 +141,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(119, 102);
+            this.label2.Location = new System.Drawing.Point(134, 102);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(174, 21);
